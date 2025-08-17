be23fe1799ec2cddaa87ce966e90744b09dda3d1
Vivek Chaturvedi <vivekchaturvedi013@gmail.com>
2025-08-18 02:32:20 +0530

    changing env

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { tap } from "rxjs";
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private api = 'http://localhost:5000/api/users';
+  private api = `${environment.apiUrl}/users`;
 
   constructor(private http: HttpClient) {}
 
